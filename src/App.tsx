b4d69f4d7fcdcb7b6a16f64e9d1de0d177b794c4
Chetan Satpute <chetan.satpute@joshsoftware.com>
2023-06-26 16:48:00 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen bg-neutral-900 flex justify-center items-center">
+      <h1 className="text-2xl text-neutral-200 font-bold">React Web App</h1>
+    </div>
+  );
+}
+
+export default App;
