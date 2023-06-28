ec23b2e1b302a3ec10b94984e2e4c28e62192b3b
Mrunmai <mrunmaikudale18@gmail.com>
2023-06-28 18:14:22 +0530

    made changes in index file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="h-screen w-screen bg-neutral-900 flex justify-center items-center">
       <h1 className="text-2xl text-neutral-200 font-bold">React Web App: Mrunmai Kudale</h1>
+      <h2 className="text-2xl text-neutral-200 font-bold">Developer: Chetan Satpute</h2>
     </div>
   );
 }
